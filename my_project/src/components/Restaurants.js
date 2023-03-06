e16619fe0223d7fc14e14ea729f1f17a9eca94d3
Michelle Bravo <bravomichelle292@gmail.com>
2023-03-06 04:02:15 -0500

    Add restaurant page

--- my_project/src/components/Restaurants.js
+++ my_project/src/components/Restaurants.js
@@ -1,11 +1,34 @@
-//import './Attractions.css';
+import './css/Restaurants.css';
+import enzos from './img/enzo.webp'
+import scoops from './img/Scoops.webp'
+
 
 function Restaurants() {
   return (
-    <div className="content">
-      This page will have information on Glove World, Jellyfishing, a>
-    </div>
-  );
+    <div className='main'>
+       <div className='header'>
+          <h1>Restaurants</h1>
+             <h3>Hawkins, Indiana is home to many small town American restuarants. These restaurants
+             are located in fun areas throughout the town, including near the mayor's office and 
+             inside the luxurious Starcourt mall. </h3>
+       </div>
+       <div className="enzos">
+             <img src={enzos} alt="enzo's restaurant" />
+             <p> Enzos Restaurant is a classic American Italian restaurant. Enzos is perfect for date nights and for family outings.
+                 Feel free to add this location to your iternary when you visit Hawkins.
+             </p>
+       </div>
+
+       <div className="scoops">
+          <img src={scoops} alt="scoops ahoy" />
+             <p> Scoops Ahoy is an ice cream classic destination for cool pirates like you.
+                 With friendly customer service and free ice cream samples,
+                 you will never want to leave Starcourt Mall.
+             </p>
+       </div>
+
+   </div>
+);
 }
 
 export default Restaurants;
